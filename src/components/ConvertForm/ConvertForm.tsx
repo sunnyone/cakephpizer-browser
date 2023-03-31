21cab0ede1e20bcd0cd0c3722e993feeca30cdd2
Yoichi Imai <sunnyone41@gmail.com>
2023-04-01 05:15:33 +0900

    Add a form handler

--- src/components/ConvertForm/ConvertForm.tsx
+++ src/components/ConvertForm/ConvertForm.tsx
@@ -1,7 +1,5 @@
 import {
   Button,
-  Checkbox,
-  CheckboxGroup,
   FormControl,
   FormLabel,
   HStack,
@@ -15,40 +13,74 @@ import {
   RadioGroup,
   Stack,
 } from "@chakra-ui/react";
-import React from "react";
+import React, {useCallback} from "react";
+import {Controller, useForm} from "react-hook-form";
+
+export interface ConvertFormValues {
+  files: File[];
+  type: "cakephpize" | "soudaize";
+  threshold: number;
+}
 
 export const ConvertForm: React.FC = () => {
+  const {
+    register,
+    control,
+    formState: {errors},
+    handleSubmit,
+  } = useForm<ConvertFormValues>();
+
+  const onSubmit = useCallback((values: ConvertFormValues) => {
+    console.log(values);
+  }, []);
+
   return (
-    <Stack>
-      <FormControl>
-        <FormLabel>画像</FormLabel>
-        <Input type="file" />
-      </FormControl>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Stack>
+        <FormControl isInvalid={!!errors.files}>
+          <FormLabel>画像</FormLabel>
+          <Input type="file" {...register("files", {required: true})} />
+        </FormControl>
 
-      <FormControl>
-        <FormLabel>タイプ</FormLabel>
-        <RadioGroup>
-          <HStack>
-            <Radio defaultChecked value="cakephpize">
-              cakephpize
-            </Radio>
-            <Radio value="soudaize">soudaize</Radio>
-          </HStack>
-        </RadioGroup>
-      </FormControl>
+        <FormControl isInvalid={!!errors.type}>
+          <FormLabel>タイプ</FormLabel>
+          <Controller
+            name="type"
+            control={control}
+            rules={{required: true}}
+            render={({field: {onChange, onBlur, value}}) => (
+              <RadioGroup value={value} onChange={onChange} onBlur={onBlur}>
+                <HStack>
+                  <Radio defaultChecked value="cakephpize">
+                    cakephpize
+                  </Radio>
+                  <Radio value="soudaize">soudaize</Radio>
+                </HStack>
+              </RadioGroup>
+            )}
+          />
+        </FormControl>
 
-      <FormControl>
-        <FormLabel>閾値(50〜80)</FormLabel>
-        <NumberInput defaultValue={50}>
-          <NumberInputField />
-          <NumberInputStepper>
-            <NumberIncrementStepper />
-            <NumberDecrementStepper />
-          </NumberInputStepper>
-        </NumberInput>
-      </FormControl>
+        <FormControl isInvalid={!!errors.type}>
+          <FormLabel>閾値(50〜80)</FormLabel>
+          <Controller
+            name="threshold"
+            control={control}
+            rules={{required: true}}
+            render={({field: {onChange, onBlur, value}}) => (
+              <NumberInput defaultValue={50}>
+                <NumberInputField />
+                <NumberInputStepper>
+                  <NumberIncrementStepper />
+                  <NumberDecrementStepper />
+                </NumberInputStepper>
+              </NumberInput>
+            )}
+          />
+        </FormControl>
 
-      <Button>変換</Button>
-    </Stack>
+        <Button type="submit">変換</Button>
+      </Stack>
+    </form>
   );
 };
